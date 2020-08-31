092074710e9783f53b20a1481df70c1424f6d787
dem4ply <dem4ply@gmail.com>
2020-08-31 15:12:02 -0500

    config

---
+++ config.py
@@ -0,0 +1,39 @@
+import sys
+from chibi.config import configuration
+from chibi.file import Chibi_path
+from chibi.module import import_
+
+sys.path.append( Chibi_path( '.' ).inflate )
+
+from containers.base import Centos_7
+from containers.elasticsearch import Misuzu, Pitou, Rei, Rem, Sakura
+from containers.nginx import Ikaros, Astraea, Caos, Nymph
+from containers.mariadb import Chii, Freya, Sumomo
+from containers.rabbitmq import Chino, Cocoa, Rize
+
+from containers.django.corona_chan import Corona_chan
+
+
+configuration.chibi_lxc.containers.add( Centos_7 )
+configuration.chibi_lxc.containers.add( Misuzu )
+configuration.chibi_lxc.containers.add( Pitou )
+configuration.chibi_lxc.containers.add( Rei )
+configuration.chibi_lxc.containers.add( Rem )
+configuration.chibi_lxc.containers.add( Sakura )
+
+configuration.chibi_lxc.containers.add( Ikaros )
+configuration.chibi_lxc.containers.add( Astraea )
+configuration.chibi_lxc.containers.add( Caos )
+configuration.chibi_lxc.containers.add( Nymph )
+
+configuration.chibi_lxc.containers.add( Chii )
+configuration.chibi_lxc.containers.add( Freya )
+configuration.chibi_lxc.containers.add( Sumomo )
+
+configuration.chibi_lxc.containers.add( Chino )
+configuration.chibi_lxc.containers.add( Cocoa )
+configuration.chibi_lxc.containers.add( Rize )
+
+configuration.chibi_lxc.containers.add( Corona_chan )
+
+configuration.chibi_lxc.hosts = Chibi_path( 'hosts' )
