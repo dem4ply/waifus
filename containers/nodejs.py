039936642f7e215460d97fd6033010bd5b08e925
dem4ply <dem4ply@gmail.com>
2021-10-26 15:32:07 -0500

    Olla tiznada, bien es guisada.

--- containers/nodejs.py
+++ containers/nodejs.py
@@ -10,8 +10,8 @@ class Nodejs( Centos_7 ):
             'git@github.com:AptudeSiGRHA/sigrha-react.git', 'main' ),
         'nodejs/install.sh',
         (
-            "provision/nodejs/provison_repo.sh",
-            '/home/chibi/projects/sigrha-react__master' ),
+            "nodejs/provison_repo.sh",
+            '/home/chibi/projects/sigrha-react__main/' ),
         ( "systemd/cp.py", 'nodejs/sigrha_react.service' ),
         ( "systemd/systemd.py", 'enable', 'sigrha_react.service' ),
         ( "systemd/systemd.py",'start', 'sigrha_react.service' ),
