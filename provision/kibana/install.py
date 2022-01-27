335cb09bc110dc88dde48a48e7e203b0230057c2
dem4ply <dem4ply@gmail.com>
2022-01-27 04:57:44 -0600

    separacion de responsabilidades en kiabana

--- provision/kibana/install.py
+++ provision/kibana/install.py
@@ -11,16 +11,13 @@ file_check_path = Chibi_path( '~/provision_installed' )
 file_check = file_check_path.open()
 
 
-version_to_check = "kibana\n".format( file=__file__, )
+version_to_check = "kibana\n"
 
 
-if __name__ == "__main__" and not version_to_check in file_check:
+if __name__ == "__main__" and version_to_check not in file_check:
     cowsay( "Starting install for kibana" )
 
     Yum.install( 'kibana' )
-
-    Systemctl.enable( "kibana" ).run()
-    Systemctl.start( "kibana" ).run()
 
     file_check.append( version_to_check )
     cowsay( "Ending install for kibana" )
--- provision/kibana/start.py
+++ provision/kibana/start.py
@@ -5,6 +5,7 @@ from chibi.config import basic_config
 from chibi.file import Chibi_path
 from chibi_command.echo import cowsay
 from chibi_command.nix import Systemctl
+from pprint import pprint
 
 
 basic_config()
@@ -14,6 +15,8 @@ provision_folder = Chibi_path( os.environ[ 'PROVISION_PATH' ] )
 provision_folder = provision_folder + 'elasticsearch' + 'provision'
 
 kibana = provision_folder + "kibana.yml"
+kibana_config = kibana.open().read()
+pprint( kibana_config )
 kibana.copy( '/etc/kibana/kibana.yml' )
 
 Systemctl.restart( "kibana" ).run()
