838e595518adf8f9f2ff3e0e1c2a3691973326b4
dem4ply <dem4ply@gmail.com>
2025-03-13 00:33:46 -0600

    actualizacion de maria para rocky 9

--- provision/mariadb/install.py
+++ provision/mariadb/install.py
@@ -7,6 +7,7 @@ from chibi.file import Chibi_path
 from chibi_command.centos import Yum, Firewall_cmd
 from chibi_command.db import Mysql
 from chibi_command.nix import Systemctl
+from chibi_atlas import Chibi_atlas
 
 
 basic_config()
@@ -17,25 +18,54 @@ file_check = file_check_path.open()
 version_to_check = "mariadb\n".format( file=__file__, )
 
 remove_root = """
-UPDATE mysql.user SET Password=PASSWORD('password') WHERE User='root';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DELETE FROM mysql.user WHERE User='';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('password');
+
 DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
-CREATE USER 'dem4ply'@'%' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'dem4ply'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'dem4ply'@'%';
 FLUSH PRIVILEGES;
 """
 
+
+def do_query( mysql_captive, query ):
+    result = mysql_captive.run_script( query )
+    rows = result.result.split( '\n' )
+    columns = rows[0].split( '\t' )
+    result = []
+    for row in rows[1:]:
+        row = row.split( '\t' )
+        result.append( Chibi_atlas( zip( columns, row ) ) )
+    return result
+
+
+def delete_empty_users( mysql, mysql_captive ):
+    query = "SELECT User, Host FROM mysql.user where User=''"
+    users = do_query( mysql_captive, query )
+    for user in users:
+        mysql.run_script( "DROP USER '{user.user}'@'{user.host}'" )
+
+    query = (
+        "DELETE FROM mysql.user "
+        "WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+    )
+    users = do_query( mysql_captive, query )
+    for user in users:
+        mysql.run_script( "DROP USER '{user.user}'@'{user.host}'" )
+
+
 if __name__ == "__main__" and not version_to_check in file_check:
     cowsay( "iniciando instalacion de mariadb" )
-    Yum( 'module', 'enable', 'mariadb:10.5' ).run()
+    Yum( 'module', 'enable', 'mariadb:10.11' ).run()
     Yum.install( "mariadb-server" )
     Systemctl.enable( "mariadb" ).run()
     Systemctl.start( "mariadb" ).run()
 
     mysql = Mysql.user( "root" )
+    mysql_captive = Mysql.user( "root" )
+    mysql_captive.captive = True
     result = mysql.run_script( remove_root )
     print( vars( result ) )
+    delete_empty_users( mysql, mysql_captive )
 
     Firewall_cmd.add_port( '3306', kind='tcp', permanent=True )
 
