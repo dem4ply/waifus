6ed28ebc41e8dc9367dd6a1e986a594156467d8a
dem4ply <dem4ply@gmail.com>
2025-03-13 00:34:16 -0600

    actualizzacion de rabbitmq para rocky 9

--- provision/rabbitmq/install.py
+++ provision/rabbitmq/install.py
@@ -5,7 +5,7 @@ from chibi.config import basic_config
 from chibi.file import Chibi_file, Chibi_path
 from chibi.file.temp import Chibi_temp_path
 from chibi_command import Command
-from chibi_command.centos import Yum, Firewall_cmd
+from chibi_command.centos import Yum, Firewall_cmd, Dnf
 from chibi_command.nix import Systemctl
 from chibi_requests import Chibi_url
 
@@ -20,21 +20,32 @@ version_to_check = "rabbitmq\n".format( file=__file__, )
 
 if __name__ == "__main__" and not version_to_check in file_check:
     cowsay( "instalando rabbitmq" )
-    RPM.rpm_import( 'https://artifacts.elastic.co/GPG-KEY-elasticsearch' )
+    RPM.rpm_import(
+        'https://github.com/rabbitmq/signing-keys/releases/download/3.0/'
+        'rabbitmq-release-signing-key.asc' )
+    RPM.rpm_import(
+        'https://github.com/rabbitmq/signing-keys/releases/download/3.0/'
+        'cloudsmith.rabbitmq-erlang.E495BB49CC4BBE5B.key' )
+    RPM.rpm_import(
+        'https://github.com/rabbitmq/signing-keys/releases/download/3.0/'
+        'cloudsmith.rabbitmq-server.9F4587F226208342.key' )
     download_path = Chibi_temp_path()
-    rebbitmq_rpm = Chibi_url(
-        "https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.1/"
-        "rabbitmq-server-3.6.1-1.noarch.rpm" ).download( path=download_path )
+    # rebbitmq_rpm = Chibi_url(
+    #    "https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.1/"
+    #    "rabbitmq-server-3.6.1-1.noarch.rpm" ).download( path=download_path )
 
-    Yum.local_install( rebbitmq_rpm )
+    Dnf.install( 'socat', 'logrotate', 'erlang', 'rabbitmq-server' )
+    # Yum.local_install( rebbitmq_rpm )
 
+    """
     Systemctl.enable( 'firewalld' ).run()
     Systemctl.start( 'firewalld' ).run()
 
-    #ports = [ "8883", "61613-61614", "15672", "5671-5672", "25672", "4369" ]
-    #for port in ports:
-    #    Firewall_cmd.add_port( ports=port ).run()
-    #Firewall_cmd.reload()
+    ports = [ "8883", "61613-61614", "15672", "5671-5672", "25672", "4369" ]
+    for port in ports:
+        Firewall_cmd.add_port( ports=port ).run()
+    Firewall_cmd.reload()
+    """
 
     Command( 'setsebool', "-P", "nis_enabled", "1" )
     Systemctl.enable( 'rabbitmq-server' ).run()
