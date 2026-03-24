7e20025eda20cda23afd7706d774b29ca48bebd0
dem4ply <dem4ply@gmail.com>
2026-03-24 05:15:23 -0600

    se elimina src

--- src/goblin/celery.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from celery import Celery
-
-goblin_slayer = Celery( 'goblin_task',
-                        broker='amqp://goblin:asdf@fumika/goblin_vhost',
-                        backend='rpc://', include=[ 'goblin.task' ] )
